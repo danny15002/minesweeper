fd88b0899a6db4ee8b05aabd116abc117a36ea9c
App Academy Student <student@appacademy.io>
2015-08-28 13:31:13 -0700

    create move and render logic

--- board.rb
+++ board.rb
@@ -2,15 +2,17 @@ require_relative 'tile'
 
 class Board
 
+  attr_reader :grid
+
   def initialize(grid = Array.new(9) {Array.new(9)})
     @grid = grid
 
   end
 
   def populate_grid
-    @grid.each do |row|
-      row.each do |col|
-        grid[row][col] = Tile.new(self)
+    @grid.each_with_index do |row, i|
+      row.each_index do |col|
+        row[col] = Tile.new(self, [i,col])
       end
     end
   end
@@ -26,6 +28,29 @@ class Board
         bomb_count += 1
       end
     end
+  end
 
+  def render
+    @grid.each do |row|
+      row.each_index do |col|
+        print "#{row[col].state} "
+      end
+      puts ""
+    end
+  end
+
+  def make_move(pos)
+    row = pos[0] - 1
+    col = pos[1] - 1
+
+    @grid[row][col].reveal
   end
 end
+
+board = Board.new
+board.populate_grid
+board.render
+board.seed_bombs
+board.make_move([3,3])
+system 'clear'
+board.render
