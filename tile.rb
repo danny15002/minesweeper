e2e7978e668cc4bb0fa35b0c21568cbef8b654d1
App Academy Student <student@appacademy.io>
2015-08-28 13:30:35 -0700

    add logic for reveal and neighbors

--- tile.rb
+++ tile.rb
@@ -1,26 +1,57 @@
 class Tile
 
-  attr_accessor :bombed
-  
-  def initialize(board)
+  attr_accessor :bombed, :revealed, :state
+
+  def initialize(board, coordinates)
     @bombed = false
     @flagged = false
     @revealed = false
     @state = "*"
     @board = board
+    @row = coordinates[0]
+    @col = coordinates[1]
   end
 
-  def reaveal
+  def reveal
+    @revealed = true
+    if @bombed
+      @state = "B"
+      return ""
+    else
+      @state = " "
+    end
+
+    neighbors
+    @neighbors.each do |neighbor| #neighbor is tile object
+      neighbor.reveal unless (neighbor_bomb_count > 0)
+    end
+
   end
 
   def neighbors
+    @neighbors = []
+    -1.upto(1) do |i|
+      -1.upto(1) do |j|
+        check_row = @row + i
+        check_col = @col + j
+        check_tile = @board.grid[check_row][check_col]
+        if check_row > 0 && check_col > 0
+          @neighbors << check_tile unless check_tile.revealed #neighboring tiles get placed into @neighbors
+        end
+      end
+    end
+    puts @neighbors.to_s
   end
 
   def neighbor_bomb_count
+    bomb_count = 0
+    @neighbors.each {|neigh_tile| bomb_count += 1 if neigh_tile.bombed}
+    bomb_count
   end
 
   def inspect
 
   end
+
 
 end
